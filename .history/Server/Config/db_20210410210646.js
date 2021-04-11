186c39f863615755eccc04225908040e2c5f8d53
Yiu Wing Lau <yiuwing.lau@dcmail.ca>
2021-04-10 21:27:39 -0400

    Final commit'

--- .history/Server/Config/db_20210410210646.js
+++ .history/Server/Config/db_20210410210646.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ConnectionType = exports.RemoteURI = exports.Secret = exports.localURI = void 0;
 exports.localURI = "mongodb://localhost/webd6201";
 exports.Secret = "someSecret";
-exports.RemoteURI = "mongodb+srv://Nelson:BfdKvS3T7Dlw8hLm@webd6201.kbwai.mongodb.net/WEBD6201?retryWrites=true&w=majority";
+exports.RemoteURI = "mongodb+srv://thomas:0xHN3GappBBYrAfh@cluster0.thzq9.azure.mongodb.net/webd6201?retryWrites=true&w=majority";
 exports.ConnectionType = "MongoDB Atlas";
 //# sourceMappingURL=db.js.map
